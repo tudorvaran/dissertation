6b4071da820941f5ad755d56dfc10190fcd59dfb
tudorvaran <tudor@sectorlabs.ro>
2020-04-25 20:31:12 +0300

    Update requirements

--- dissertation/settings.py
+++ dissertation/settings.py
@@ -79,6 +79,17 @@ DATABASES = dict(
     default=dj_database_url.config(default="postgres:///dissertation")
 )
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PARSER_CLASS": "redis.connection.HiredisParser",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
